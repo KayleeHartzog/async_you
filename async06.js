de88560ba41dfaaeb9581f250eef5f4377495df8
KayleeHartzog <har15031@byui.edu>
2019-06-07 12:52:39 -0600

    finised async06.js

--- async06.js
+++ async06.js
@@ -1,11 +1,37 @@
 const http = require('http')
 const async = require('async')
-const url1 = process.argv[2]
+const url = require('url')
+const myUrl = process.argv[2]
 
-http.get(url, function(res) {
-    async.reduce(['one', 'two', 'three'], 0, function() {
-
-    }, function(err, results) {
-        console.log(results)
+async.reduce(['one', 'two', 'three'], 0, function(memo, item, callback) {
+    http.get(myUrl + '?number=' + item, function(res) {
+        var body = ''
+        res.on('data', function(chunk) {
+            body += chunk.toString()
+        })
+        res.on('end', function() {
+            callback(null, memo + parseInt(body))
+        })
     })
+}, function(err, results) {
+    console.log(results)
 })
+
+
+// var numbers = ['one', 'two', 'three']
+// var newUrl = url.parse(myUrl)
+
+// var acc = 0;
+// numbers.forEach(function(number) {
+//     newUrl.query = {"number" : number}
+//     console.log(newUrl)
+//     http.get(newUrl, function(res) {
+//         res.on('data', function(chunk) {
+//             async.reduce([chunk], acc, function(memo, item) {
+//                 acc = memo + item
+//             })
+//         })
+//     }).end()
+// })
+
+// console.log(acc)
